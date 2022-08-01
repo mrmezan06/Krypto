a1ab8ff117f45ae38c5e1533ccea9acd9a1de2a2
Mejanur Rahman <mrmezan06@gmail.com>
2022-08-01 12:20:16 +0600

    Transaction History with Dummy data completed

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -3,8 +3,12 @@ import { TransactionContext } from '../context/TransactionContext';
 
 import dummyData from '../utils/dummyData';
 import { shortenAddress } from '../utils/shortenAddress';
+import useFetch from '../hooks/useFetch';
 
 const TransactionCard = ({ addressTo, addressFrom, timestamp, message, keyword, amount, url }) => {
+  
+  const gifUrl = useFetch({ keyword });
+  
   return (
     <div className="bg-[#181918] m-4 flex flex-1
     2xl:min-w-[450px]
@@ -36,11 +40,17 @@ const TransactionCard = ({ addressTo, addressFrom, timestamp, message, keyword, 
                   <p className="text-white text-base">Message: {message}</p>
                   </>
                 )}
+                </div>
+
+                <img 
+                  src={gifUrl || url}
+                  alt="gif"
+                  className="w-full h-64 2xl:h-96 rounded-md shadow-lg object-cover"
+                />
                 <div className="bg-black p-3 px-5 w-max rounded-3xl -mt-5 shadow-2xl">
                   <p className="text-[#37c7da] font-bold">
                     {timestamp}
                   </p>
-                </div>
              </div>
          </div>
     </div>
