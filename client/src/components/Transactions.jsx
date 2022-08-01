9ee1fd0afc04aa6877cbb180465df7fa5f20d34d
Mejanur Rahman <mrmezan06@gmail.com>
2022-08-01 11:56:56 +0600

    Adding ENV file and New Hook for fetch

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -1,8 +1,78 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { TransactionContext } from '../context/TransactionContext';
+
+import dummyData from '../utils/dummyData';
+import { shortenAddress } from '../utils/shortenAddress';
+
+const TransactionCard = ({ addressTo, addressFrom, timestamp, message, keyword, amount, url }) => {
+  return (
+    <div className="bg-[#181918] m-4 flex flex-1
+    2xl:min-w-[450px]
+    2xl:max-w-[500px]
+    sm:min-w-[270px]
+    sm:max-w-[300px]
+    flex-col p-3 rounded-md hover:shadow-2xl
+    ">
+         <div className="flex flex-col items-center w-full mt-3">
+             <div className="w-full mb-6 p-2">
+                <a href={`https://goerli.etherscan.io/address/${addressFrom}`}
+                   target="_blank" rel="noopener noreferrer">
+                      <p className="text-white text-base">
+                          From: {shortenAddress(addressFrom)}
+                      </p>
+                </a>
+                <a href={`https://goerli.etherscan.io/address/${addressTo}`}
+                   target="_blank" rel="noopener noreferrer">
+                      <p className="text-white text-base">
+                          To: {shortenAddress(addressTo)}
+                      </p>
+                </a>
+                <p className="text-white text-base">
+                    Amount: {amount} ETH
+                </p>
+                {message && (
+                  <>
+                  <br />
+                  <p className="text-white text-base">Message: {message}</p>
+                  </>
+                )}
+                <div className="bg-black p-3 px-5 w-max rounded-3xl -mt-5 shadow-2xl">
+                  <p className="text-[#37c7da] font-bold">
+                    {timestamp}
+                  </p>
+                </div>
+             </div>
+         </div>
+    </div>
+  );
+}
 
 const Transactions = () => {
+  const { currentAccount } = useContext(TransactionContext);
   return (
-    <div>Transactions</div>
+    <div className="flex w-full justify-center items-center 2xl:px-20 gradient-bg-transactions">
+        <div className="flex flex-col md:p-12 py-12 px-4">
+          {/* Latest Transactions */}
+          {currentAccount ? (
+            <h3 className="text-white text-3xl text-center my-2">
+              Latest Transactions
+            </h3>
+          )
+          :
+          {/* Connect your account to see the latest transactions */}
+          (
+            <h3 className="text-white text-3xl text-center my-2">
+              Connect your account to see the latest transactions
+            </h3>
+          )}
+          {/* Loop through all of the transactions */}
+            <div className="flex flex-wrap justify-center items-center mt-10">
+              {dummyData.reverse().map((transaction, i) => (
+                <TransactionCard key={i} {...transaction} />
+              ))}
+            </div>
+        </div>
+    </div>
   )
 }
 
